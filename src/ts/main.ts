bec7f58e7bcf400a499fa014e54eda0a375a8f40
Leath Cooper <leath@abovealloffers.com>
2015-10-21 13:17:46 -0700

    dummy code

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,3 +1,12 @@
 ///<reference path='./typings/jquery/jquery.d.ts' />
 
 import $ = require('jquery');
+
+class TestClass {
+  
+  constructor() {
+    $('body').addClass('tested');
+  }
+}
+
+$(new TestClass());
